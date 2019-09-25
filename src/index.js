335b291df8b6efccb2ecf022160696d668708eb2
Volha <shaukavets@gmail.com>
2019-09-25 19:06:01 +0300

    add function decoder
    add function decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let result='';
+
+    console.log(expr.length);
+    console.log(expr[0])
+    
+    for (let i=0; i<expr.length; i=i+10){
+        let codedLetter='';
+        let encodedLetter;
+        for (let j=i; j<i+10; j=j+2){
+                if (expr[j]+expr[j+1]=='10'){
+                    codedLetter = codedLetter+".";
+                }
+                else if (expr[j]+expr[j+1]=='11') {
+                    codedLetter = codedLetter+"-";
+                }
+                else if (expr[j]+expr[j+1]=='**') {
+                    encodedLetter = " ";
+            }
+            console.log(codedLetter);
+        }
+        if (encodedLetter!=" "){
+            encodedLetter = MORSE_TABLE[codedLetter];
+        }
+        result=result+encodedLetter;
+    }
+return result;
 }
 
 module.exports = {
